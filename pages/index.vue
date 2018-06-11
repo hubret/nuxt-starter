77324f37e38f047666ac346558239218712b1f2c
Hubert <hubhcsi@gmail.com>
2018-06-11 16:01:23 -0400

    add files

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello world!</h1>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                title: 'Hello World!'
+            }
+        },
+        head () {
+            return {
+                title: this.title,
+                meta: [
+                    { hid: 'og:title', name: 'og:title', content: this.title },
+                    { hid: 'og:title', name: 'og:title', content: this.title },
+                    { hid: 'description', name: 'description', content: 'My custom description' }
+                ]
+            }
+        }
+    }
+</script>
